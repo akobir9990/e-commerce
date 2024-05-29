25020af8e5b6f0a86f015aa3d641a9436135e69b
akobir9990 <akobir.qoshshayev@gmail.com>
2024-05-29 12:57:53 +0500

    tilwind config: added: -colors, -screen sizes, fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import "./index.css";
+
 export default function App() {
-  return <div className="text-[teal]">hey</div>;
+  return <div className="text-crossedOutTextColor underline">hello</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      fontBold: `ptRootUI-700`,
+      fontMedium: `ptRootUI-500`,
+      fontRegular: `ptRootUI-400`,
+      fontLight: `ptRootUI-300`,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      primaryColor: "#5956CE",
+      salePersentBgColor: "##e44542",
+      crossedOutTextColor: "#999999",
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [],
 };
