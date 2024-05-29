0d4680a4f06eb0857ea8a2b142e66a6457e92c68
akobir9990 <akobir.qoshshayev@gmail.com>
2024-05-29 12:28:32 +0500

    download icons

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div>
+      <div className="container">
+        <div className="h1"></div>
+        <div className="socials">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const socials = [
+  { id: 1, title: "telegram", url: "" },
+  { id: 2, title: "vkontakte", url: "" },
+  { id: 3, title: "odnoklasniki", url: "" },
+];
