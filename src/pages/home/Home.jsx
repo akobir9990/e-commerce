678665592eb4b338a233c76d6f0eb7e374410f91
akobir9990 <akobir.qoshshayev@gmail.com>
2024-05-29 15:44:28 +0500

    created  Home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import CardItem from "../../components/cardItem/CardItem";
+import Hero from "../../components/hero/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <CardItem />
+    </div>
+  );
+}
