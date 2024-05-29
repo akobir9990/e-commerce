30baae35f1e45b5726794317aa21fb9fc9165337
akobir9990 <akobir.qoshshayev@gmail.com>
2024-05-29 15:40:46 +0500

    create Hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,22 @@
+export default function Hero() {
+  return (
+    <div className="">
+      <div className="container py-[111px]">
+        <h1 className="text-[64px] font-[ptRootUI-700]">
+          Натуральный стиль и: <br /> экологичность очки из дерева
+        </h1>
+        <p className="text-[24px] max-w-[760px] leading-8 font-[ptRootUI-500] my-4">
+          Откройте для себя уникальность деревянных очков. Созданные с заботой о
+          природе и вашем комфорте, наши очки - это сочетание инновационного
+          дизайна и природных материалов. Каждая пара очков ручной работы
+          является отражением вашей любви к планете и стремления к
+          оригинальности.
+        </p>
+
+        <button className="bg-primaryColor px-5 py-2 text-[20px] text-[white] flex items-center justify-center rounded-[1px] font-[ptRootUI-300]">
+          Узнать подробнее
+        </button>
+      </div>
+    </div>
+  );
+}
