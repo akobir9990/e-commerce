1bb5b55df46502792c4a6a915b6b308bd26488c0
akobir9990 <akobir.qoshshayev@gmail.com>
2024-05-29 12:16:52 +0500

    create header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="container flex justify-between items-center py-6">
+        <div className="menu">
+          <div className="burger">burger</div>Меню
+        </div>
+        <div className="logo">LANDING</div>
+        <div className="number">
+          <img src="" alt="" />
+          +7 777 777 77 77
+        </div>
+      </div>
+    </div>
+  );
+}
