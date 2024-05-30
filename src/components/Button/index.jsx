40dd29a2208865058765c9f5df4d7ee0a97c8ae3
akobir9990 <akobir.qoshshayev@gmail.com>
2024-05-30 11:14:33 +0500

    fix: onClick prop changed

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, style, onClick }) {
   return (
     <button
-      onClick={onClick()}
+      onClick={onClick}
       className={`bg-primaryColor text-white flex justify-center items-center w-full h-10 rounded-lg ${style}`}
     >
       {children}
